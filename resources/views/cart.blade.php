b4376ce08286401ecaa99ef6c41cf29cbf843dca
ClementFarge <clement.farge@le-campus-numerique.fr>
2022-06-13 14:42:06 +0200

    First commit Cart Branch

---
+++ resources/views/cart.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+
+<h1> PANIER </h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/cart', [Controllers\CartController::class, 'cart']);
